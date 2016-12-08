afa36fce92809ed2007423ccb63b80fbb2155acc
Gavin Joyce <gavinjoyce@gmail.com>
2016-12-08 21:12:02 +0000

    fixup!

--- src/index.js
+++ src/index.js
@@ -1,41 +1,68 @@
+import State from './state';
+
 var stringify = require('json-stable-stringify');
 
 module.exports = function(options) {
   function plugin(babel) {
+    let state = new State(options);
     var types = babel.types;
     var callPathsToRemove;
 
     return new babel.Transformer('babel-plugin-remove-functions', {
       Program: {
-        enter: function() {
-          callPathsToRemove = [];
-          options.removals.forEach(function(removal) {
-            if(removal.global) {
-              removal.methods.forEach(function(method) {
-                callPathsToRemove.push(removal.global + '.' + method);
-              });
-            }
-          });
+        enter() {
+          state.enter();
         },
-        exit: function() {
-          callPathsToRemove = undefined;
+        exit() {
+          state.exit();
+        }
+      },
+
+      ImportDeclaration: function(node) {
+        node.specifiers.forEach((specifier) => {
+          state.importSpecifier(specifier.type, node.source.value, specifier.local.name);
+        });
+      },
+
+      ExpressionStatement: function(node) {
+        if(!node.expression.callee || !node.expression.callee.name) {
+          return;
+        }
+
+        let callPath = node.expression.callee.name;
+        if(state.showRemoveCallPath(callPath)) {
+          this.dangerouslyRemove();
+        }
+      },
+
+      CallExpression: function(node) {
+        if(node.callee.type === 'MemberExpression') {
+          let callPath = getCallPath(node.callee);
+
+          if(state.showRemoveCallPath(callPath)) {
+            this.dangerouslyRemove();
+          }
         }
       },
 
       VariableDeclaration: function(node) {
-        node.declarations.forEach(function(declaration) {
+        //TODO: GJ: move some of this into State
+        
+        //eg. `const { assert, deprecate } = Ember;`
+        node.declarations.forEach((declaration) => {
           if(declaration.init) {
-            var importName = declaration.init.name; //eg. `Ember` (from `const { debug } = Ember;`)
+            let importSource = state.getImportSourceFromLocal(declaration.init.name);
 
-            options.removals.forEach(function(removal) {
-              if(removal.module === importName) {
+            options.removals.forEach((removal) => {
+              if(removal.module === importSource) {
+
                 if(declaration.id && declaration.id.properties) {
-                  declaration.id.properties.forEach(function(property) {
+                  declaration.id.properties.forEach((property) => {
                     //eg. const { warn: renamedWarn } = Ember;
                     //    =>: property.key.name => 'warn'
                     //    =>: property.value.name => 'renamedWarn'
-                    if(removal.methods.indexOf(property.key.name) !== -1) {
-                      callPathsToRemove.push(property.value.name);
+                    if(removal.paths.indexOf(property.key.name) !== -1) {
+                      state.callPathsToRemove.add(property.value.name);
                     }
                   });
                 }
@@ -43,48 +70,8 @@ module.exports = function(options) {
             });
           }
         });
-      },
-
-      ImportDeclaration: function(node) {
-        options.removals.forEach(function(removal) {
-          if(types.isLiteral(node.source, { value: removal.module })) {
-            var firstNode = node.specifiers && node.specifiers[0];
-            if(types.isImportDefaultSpecifier(firstNode)) {
-              removal.methods.forEach(function(method) {
-                callPathsToRemove.push(firstNode.local.name + '.' + method);
-              });
-            }
-          }
-        });
-      },
-
-      CallExpression: function(node) {
-        if(callPathsToRemove.length === 0) {
-          return;
-        }
-
-        if(node.callee.type === 'MemberExpression') {
-          var callPath = getCallPath(node.callee);
-
-          if(callPathsToRemove.indexOf(callPath) !== -1) {
-            this.dangerouslyRemove();
-          }
-        }
-      },
-
-      ExpressionStatement: function(node) {
-        if(callPathsToRemove.length === 0) {
-          return;
-        }
-
-        if(!node.expression.callee) {
-          return;
-        }
-
-        if(callPathsToRemove.indexOf(node.expression.callee.name) !== -1) {
-          this.dangerouslyRemove();
-        }
       }
+
     });
   };
 
@@ -109,5 +96,5 @@ function getCallPath(node) {
     };
   }
 
-  return leftSide + '.' + node.property.name;
+  return `${leftSide}.${node.property.name}`;
 }
